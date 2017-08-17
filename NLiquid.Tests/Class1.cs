6f182b3a88c5a94e0720eaa01d7c94534661206f
ifle <igor609@gmail.com>
2017-08-17 09:14:09 +0300

    Changed target of NLiquid.Tests from core to .net framework. Refactoring

---
+++ NLiquid.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Nitra;
+using NLiquid.Parser;
+using NUnit.Framework;
+
+namespace NLiquid.Tests
+{
+	[TestFixture]
+	public class Class1
+    {
+		[Test]
+		public void  WillbeRemoved()
+	    {
+		    var parseResult = Grammar.Start.Parse(new SourceSnapshot("{{hello}}"));
+			Debug.WriteLine(parseResult);
+	    }
+    }
+}
