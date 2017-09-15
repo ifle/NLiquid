d361cda4177ae68ea3886533beccfbfc80e8c366
VladD2 <vc@rsdn.ru>
2017-09-15 04:42:26 +0300

    1. Add NLiquid.Compiler.Service.csproj. 2. Add IPostprocessing support (don't forget to synchronize with Nitra).

---
+++ NLiquid.Compiler.Service/Compiler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nemerle.Builtins;
+using Nitra.Declarations;
+using Nitra.ProjectSystem;
+
+namespace NLiquid.Compiler.Service
+{
+	public class Compiler : IPostprocessing
+	{
+		public void Postprocessing(CancellationToken cancellationToken, Project project, ImmutableArray<Nemerle.Builtins.Tuple<IAst, bool>> asts, ProjectData data)
+		{
+			Console.WriteLine("Postprocessing!!!");
+		}
+	}
+}
