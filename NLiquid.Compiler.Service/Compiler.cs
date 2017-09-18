bf4275597e23a4d1c8aab99fc6bdfdfdc1ce0b95
ifle <igor609@gmail.com>
2017-09-18 14:37:32 +0300

    Emit template method

--- NLiquid.Compiler.Service/Compiler.cs
+++ NLiquid.Compiler.Service/Compiler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -12,6 +13,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.IO;
+using NLiquid.Parser.Ast;
 
 namespace NLiquid.Compiler.Service
 {
@@ -29,24 +31,47 @@ namespace NLiquid.Compiler.Service
 			AssemblyBuilder ab = ad.DefineDynamicAssembly(am, AssemblyBuilderAccess.Save);
 			ModuleBuilder mb = ab.DefineDynamicModule(name + "Mod", dllName);
 			TypeBuilder tb = mb.DefineType("NLiquidTemplates", TypeAttributes.Public);
-
+			var context = data.Context;
             foreach (var compileUnit in asts)
                 if (compileUnit.Field1)
-                    EmitTemplateMethod(compileUnit.Field0, tb);
+                    EmitTemplateMethod(compileUnit.Field0, tb, context);
 
 			tb.CreateType();
 			ab.Save(dllName);
 		}
 
-        void EmitTemplateMethod(IAst ast, TypeBuilder tb)
+        void EmitTemplateMethod(IAst ast, TypeBuilder tb, NLiquidDependentPropertyEvalContext context)
         {
-            var file = ast.Location.Source.File;
+	        TextWriter writer;
+
+			var file = ast.Location.Source.File;
             var templateName = Path.GetFileNameWithoutExtension(file.Name).Replace(".", "_").Replace("-", "_");
-            // здесь надо сформировать параметр по первому элементу АСТ, если он есть.
-            MethodBuilder methodBuilder = tb.DefineMethod(templateName, MethodAttributes.Public | MethodAttributes.Static, null, null);
-            // генерируем код
-            Expression<Action> expressionTree = () => Console.WriteLine("Здесь надо сгенерировать код");
-            expressionTree.CompileToMethod(methodBuilder);
+
+            var methodBuilder = tb.DefineMethod(templateName, MethodAttributes.Public | MethodAttributes.Static, null, null);
+			// the first parameter of tempalte method is TextWriter
+	        var methodParameters = new List<ParameterExpression>{  Expression.Parameter(typeof(TextWriter), "writer") };
+
+			// comment parameter
+			var parameter = ((CompilationUnit)ast).Parameter;
+	        if (parameter.HasValue)
+	        {
+				var dotnetType = context.PredefinedTypes.GetDotNetType(parameter.Value.TypeRef.Symbol);
+				if(dotnetType != null)
+					methodParameters.Add(Expression.Parameter(dotnetType, parameter.Value.Name.Text));
+				else
+					Debug.Assert(false, $".NET Type for {parameter.Value.TypeRef.Symbol} is null");
+	        }
+
+			var templateMethod = Expression.Lambda(
+				Expression.Block(
+						//new []  { Expression.Variable(typeof(string), "igorvar") },
+						Expression.Call(methodParameters[0],
+							typeof(TextWriter).GetMethod("WriteLine", new [] { typeof(object) } ),
+							methodParameters.Count == 2 ? (Expression) methodParameters[1] : Expression.Constant("Hello"))
+					),
+				methodParameters
+				);
+	        templateMethod.CompileToMethod(methodBuilder);
         }
     }
 }
