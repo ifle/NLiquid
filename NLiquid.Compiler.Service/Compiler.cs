8ca8963c0f08ac41ef08c0b2fba3242b12d1c5a8
ifle <igor609@gmail.com>
2017-09-27 12:39:11 +0300

    Compiling simple variables, binary expressions, if/elseif/else

--- NLiquid.Compiler.Service/Compiler.cs
+++ NLiquid.Compiler.Service/Compiler.cs
@@ -21,6 +21,7 @@ namespace NLiquid.Compiler.Service
 	public class Compiler : IPostprocessing
 	{
 		private static MethodInfo _defaultTextWriteMethodInfo = typeof(TextWriter).GetMethod("WriteLine", new[] { typeof(object) });
+		private static ParameterExpression _textWriterParameter = Expression.Parameter(typeof(TextWriter), "writer");
 		private static Dictionary<System.Type, MethodInfo> _writeTextWriteMethodInfoMap = new Dictionary<System.Type, MethodInfo>
 		{
 			{ typeof(int),     typeof(TextWriter).GetMethod("Write", new[] { typeof(int) }) },
@@ -28,6 +29,10 @@ namespace NLiquid.Compiler.Service
 			{ typeof(string),  typeof(TextWriter).GetMethod("Write", new[] { typeof(string) }) },
 			{ typeof(bool),    typeof(TextWriter).GetMethod("Write", new[] { typeof(bool) }) },
 		};
+
+		NLiquidDependentPropertyEvalContext _context;
+		Dictionary<LocalVariableSymbol,Expression> _variablesMap;
+
 
 		public void Postprocessing(CancellationToken cancellationToken, Project project, ImmutableArray<Nemerle.Builtins.Tuple<IAst, bool>> asts, ProjectData data)
 		{
@@ -54,152 +59,24 @@ namespace NLiquid.Compiler.Service
         {
 			var compilationUnit            = (CompilationUnit)ast;
 			var methodBuilder              = CreatreMethodBuilder(ast, tb);
-	        var textWriterParameter        = Expression.Parameter(typeof(TextWriter), "writer");
-			var variablesMap               = new Dictionary<LocalVariableSymbol,Expression>();
-			var statements                 = new List<Expression>();
-
-			#region EmitTextWriteStatement
-
-			void EmitTextWriteStatement<T>(T value)
-			{
-				var valueType = typeof(T);
-
-				if (!_writeTextWriteMethodInfoMap.TryGetValue(valueType, out var methodInfo))
-					methodInfo = _defaultTextWriteMethodInfo;
-				statements.Add(Expression.Call(textWriterParameter, methodInfo, Expression.Constant(value, valueType)));
-			}
-
-	        #endregion
-
-			#region EmitCommentParameter
-
-			ParameterExpression EmitCommentParameter()
-	        {
-		        ParameterExpression result = null;
-		        var parameter = compilationUnit.Parameter;
-		        if (parameter.HasValue)
-		        {
-			        var dotnetType = context.PredefinedTypes.GetDotNetType(parameter.Value.TypeRef.Symbol);
-			        if (dotnetType != null)
-			        {
-				        result = Expression.Parameter(dotnetType, parameter.Value.Name.Text);
-				        variablesMap[parameter.Symbol.Value] = result;
-			        }
-			        else
-				        Debug.Assert(false, $".NET Type for {parameter.Value.TypeRef.Symbol} is null");
-		        }
-		        return result;
-	        }
-
-		    #endregion
-
-			#region EmitStatement
-
-			void EmitStatement(Statement statement)
-	        {
-		        switch (statement)
-		        {
-			        case Statement.Plain plainStmt:
-						EmitTextWriteStatement<string>(plainStmt.Value.Value);
-				        break;
-			        case Statement.Output outputStmt:
-				        EmitTextWriteStatement<string>(outputStmt.Expr.StringValue);
-						break;
-			        case SimpleLocalVariable simpleLocalVariableStmt:
-				        break;
-			        case CaptureLocalVariable captureLocalVariableStmt:
-				        break;
-			        case Statement.Unless unlessStmt:
-				        break;
-			        case Statement.If ifStmt:
-				        break;
-			        case ElseIf elseIfStmt:
-				        break;
-			        case Else elseStmt:
-				        break;
-			        case For forStmt:
-				        break;
-			        case Statement.Break breakStmt:
-				        break;
-			        case Statement.Continue continueStmt:
-				        break;
-					default:
-						statements.Add(Expression.Empty());
-						break;
-				}
-	        }
-
-			#endregion
-
-			#region EmitExpression
-
-			Expression EmitExpression(Expr expression)
-	        {
-		        switch (expression)
-		        {
-			        case Expr.True trueExpr:
-				        return Expression.Constant(true, typeof(bool));
-			        case Expr.False falseExpr:
-				        return Expression.Constant(false, typeof(bool)); ;
-			        case Expr.Int intExpr:
-				        return Expression.Constant(intExpr.Value.Value, typeof(int));
-			        case Expr.Double doubleExpr:
-				        return Expression.Constant(doubleExpr.Value.Value, typeof(double));
-			        case Expr.SStr sStrExpr:
-				        return Expression.Constant(sStrExpr.Value.Value, typeof(string));
-			        case Expr.DStr dStrExpr:
-				        return Expression.Constant(dStrExpr.Value.Value, typeof(string)); ;
-			        case Expr.SimpleReference simpleReferenceExpr:
-				        variablesMap.TryGetValue(simpleReferenceExpr.Ref.Symbol, out var value);
-				        return value ?? Expression.Empty();
-			        case Expr.MemberAccess memberAccessExpr:
-				        break;
-			        case Expr.ArrayAccess arrayAccessExpr:
-				        break;
-			        case Expr.Call callExpr:
-				        break;
-			        case Expr.Contains containsExpr:
-				        break;
-			        case Expr.And andExpr:
-				        break;
-			        case Expr.Or orExpr:
-				        break;
-			        case Expr.Equal equalExpr:
-				        break;
-			        case Expr.NotEqual notEqualExpr:
-				        break;
-			        case Expr.Range rangeExpr:
-				        break;
-			        case Expr.Error errorExpr:
-				        break;
-		        }
-		        return Expression.Empty();
-	        }
-
-	        #endregion
-
+	        _variablesMap				   = new Dictionary<LocalVariableSymbol, Expression>();
+	        _context                       = context;
+	        //_variablesMap				   = new Dictionary<LocalVariableSymbol, Expression>(context.PredefinedTypes.DotNetTypesMap);
 			// the first parameter of tempalte method is TextWriter
-	        var methodParameters = new List<ParameterExpression>{  textWriterParameter };
+	        var methodParameters = new BlockCollector();
+	        methodParameters.AddVariable(_textWriterParameter);
 
-			var commentParameter = EmitCommentParameter();
+			var commentParameter = EmitCommentParameter(compilationUnit);
 			if(commentParameter != null)
-				methodParameters.Add(commentParameter);
+				methodParameters.AddVariable(commentParameter);
 
+			var blockCollector = new BlockCollector();
 	        foreach (var statement in compilationUnit.Statements)
 	        {
-		        EmitStatement(statement);
+		        EmitStatement(statement, blockCollector);
 	        }
 
-			if(statements.Count == 0)
-				statements.Add(Expression.Empty());
-
-	        var templateMethod = Expression.Lambda(
-				Expression.Block(
-						//new []  { Expression.Variable(typeof(string), "igorvar") },
-						statements
-					),
-				methodParameters
-				);
+	        var templateMethod = Expression.Lambda(blockCollector.ToBlockExpression(), methodParameters.Variables);
 	        templateMethod.CompileToMethod(methodBuilder);
         }
 
@@ -210,6 +87,162 @@ namespace NLiquid.Compiler.Service
 
 			var methodBuilder = tb.DefineMethod(templateName, MethodAttributes.Public | MethodAttributes.Static, null, null);
 			return methodBuilder;
+		}
+
+		private ParameterExpression EmitCommentParameter(CompilationUnit compilationUnit)
+		{
+			ParameterExpression result = null;
+			var parameter = compilationUnit.Parameter;
+			if (parameter.HasValue)
+			{
+				var dotnetType = _context.PredefinedTypes.GetDotNetType(parameter.Value.TypeRef.Symbol);
+				if (dotnetType != null)
+				{
+					result = Expression.Parameter(dotnetType, parameter.Value.Name.Text);
+					_variablesMap[parameter.Symbol.Value] = result;
+				}
+				else
+					Debug.Assert(false, $".NET Type for {parameter.Value.TypeRef.Symbol} is null");
+			}
+			return result;
+		}
+
+		private Expression EmitExpression(Expr expression)
+		{
+			switch (expression)
+			{
+				case Expr.True trueExpr:
+					return Expression.Constant(true, typeof(bool));
+				case Expr.False falseExpr:
+					return Expression.Constant(false, typeof(bool)); ;
+				case Expr.Int intExpr:
+					return Expression.Constant(intExpr.Value.Value, typeof(int));
+				case Expr.Double doubleExpr:
+					return Expression.Constant(doubleExpr.Value.Value, typeof(double));
+				case Expr.SStr sStrExpr:
+					return Expression.Constant(sStrExpr.Value.Value, typeof(string));
+				case Expr.DStr dStrExpr:
+					return Expression.Constant(dStrExpr.Value.Value, typeof(string)); ;
+				case Expr.SimpleReference simpleReferenceExpr:
+					_variablesMap.TryGetValue(simpleReferenceExpr.Ref.Symbol, out var value);
+					return value ?? Expression.Empty();
+				case Expr.MemberAccess memberAccessExpr:
+					break;
+				case Expr.ArrayAccess arrayAccessExpr:
+					break;
+				case Expr.Call callExpr:
+					break;
+				case Expr.Contains containsExpr:
+					break;
+				case Expr.And andExpr:
+					return Expression.And(EmitExpression(andExpr.Expr1), EmitExpression(andExpr.Expr2));
+				case Expr.Or orExpr:
+					return Expression.And(EmitExpression(orExpr.Expr1), EmitExpression(orExpr.Expr2));
+				case Expr.Equal equalExpr:
+					return Expression.Equal(EmitExpression(equalExpr.Expr1), EmitExpression(equalExpr.Expr2));
+				case Expr.NotEqual notEqualExpr:
+					return Expression.NotEqual(EmitExpression(notEqualExpr.Expr1), EmitExpression(notEqualExpr.Expr2));
+				case Expr.Range rangeExpr:
+					break;
+				case Expr.Error errorExpr:
+					break;
+			}
+			return Expression.Empty();
+		}
+
+		private BlockCollector EmitStatement(IEnumerable<Statement> statements, BlockCollector blockCollector = null)
+		{
+			if(blockCollector == null)
+				blockCollector = new BlockCollector();
+			foreach (var statement in statements)
+			{
+				EmitStatement(statement, blockCollector);
+			}
+
+			return blockCollector;
+		}
+		private void EmitStatement(Statement statement, BlockCollector blockCollector)
+		{
+			switch (statement)
+			{
+				case Statement.Plain plainStmt:
+					EmitTextWriteStatement<string>(plainStmt.Value.Value, blockCollector);
+					break;
+				case Statement.Output outputStmt:
+					EmitTextWriteStatement<string>(outputStmt.Expr.StringValue, blockCollector);
+					break;
+				case SimpleLocalVariable simpleLocalVariableStmt:
+					EmitSimpleLocalVariableStatement(simpleLocalVariableStmt, blockCollector);
+					break;
+				case CaptureLocalVariable captureLocalVariableStmt:
+					break;
+				case Statement.Unless unlessStmt:
+					break;
+				case Statement.If ifStmt:
+					EmitIfStatement(ifStmt, blockCollector);
+					break;
+				case For forStmt:
+					break;
+				case Statement.Break breakStmt:
+					break;
+				case Statement.Continue continueStmt:
+					break;
+				default:
+					blockCollector.AddExpression(Expression.Empty());
+					break;
+			}
+		}
+
+		private void EmitTextWriteStatement<T>(T value, BlockCollector blockCollector)
+		{
+			var valueType = typeof(T);
+
+			if (!_writeTextWriteMethodInfoMap.TryGetValue(valueType, out var methodInfo))
+				methodInfo = _defaultTextWriteMethodInfo;
+			blockCollector.AddExpression(Expression.Call(_textWriterParameter, methodInfo, Expression.Constant(value, valueType)));
+		}
+
+		private void EmitSimpleLocalVariableStatement(SimpleLocalVariable simpleLocalVariableStmt, BlockCollector blockCollector)
+		{
+			if(_context.PredefinedTypes.DotNetTypesMap.TryGetValue(simpleLocalVariableStmt.Symbol.Type, out var type))
+			{
+				var variable = Expression.Variable(type, simpleLocalVariableStmt.Name.Text);
+				var assingExpr = Expression.Assign(variable, EmitExpression(simpleLocalVariableStmt.Initializer));
+				blockCollector.AddVariable(variable).AddExpression(assingExpr);
+
+				_variablesMap[simpleLocalVariableStmt.Symbol] = variable;
+			}
+
+		}
+
+		private void EmitIfStatement(Statement.If ifStmt, BlockCollector blockCollector)
+		{
+			var ifStmtElseIfs     = (IAstList<ElseIf>) ifStmt.ElseIfs;
+			var elseBody          = ifStmt.Else.HasValue ? EmitStatement(ifStmt.Else.Value.Body).ToBlockExpression() : null;
+			Expression elseIfExpr = null;
+			// else if statements
+			if(ifStmt.ElseIfs != null && ifStmtElseIfs.Count > 0)
+			{
+				var lastElseIf = ifStmtElseIfs[ifStmtElseIfs.Count - 1];
+				Expression lastElseIfExpr = elseBody == null
+					? Expression.IfThen(EmitExpression(lastElseIf.Condition), EmitStatement(lastElseIf.Body).ToBlockExpression())
+					: Expression.IfThenElse(EmitExpression(lastElseIf.Condition), EmitStatement(lastElseIf.Body).ToBlockExpression(), elseBody);
+
+				for (int i = ifStmtElseIfs.Count - 2; i >= 0; i--)
+				{
+					var currentElseIf = ifStmtElseIfs[i];
+					elseIfExpr = Expression.IfThenElse(
+												EmitExpression(currentElseIf.Condition), EmitStatement(currentElseIf.Body).ToBlockExpression(),
+												Expression.Block(lastElseIfExpr));
+					lastElseIfExpr = elseIfExpr;
+				}
+
+				elseBody = Expression.Block(elseIfExpr ?? lastElseIfExpr);
+			}
+
+			blockCollector.AddExpression(elseBody == null
+				? Expression.IfThen(EmitExpression(ifStmt.Condition), EmitStatement(ifStmt.Body).ToBlockExpression())
+				: Expression.IfThenElse(EmitExpression(ifStmt.Condition), EmitStatement(ifStmt.Body).ToBlockExpression(), elseBody));
 		}
 	}
 }
